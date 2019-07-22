d1dccdc75a6f7d55be9a925aabb5682242ad1dc1
Cyril POIDEVIN <vilsafur@gmail.com>
2019-07-22 21:23:31 +0200

    style: indentation

--- src/controllers/registerController.ts
+++ src/controllers/registerController.ts
@@ -24,52 +24,52 @@ export default class registerController {
     const data = request.body as RequestRegister;
     let errors: string[] = [];
 
-      if (data !== undefined) {
-        if (data.serviceName === undefined) {
-          errors.push('No servicename defined');
-        }
-  
-        if (data.version === undefined) {
-          errors.push('No version defined');
-        }
-  
-        if (data.ip === undefined) {
-          errors.push('No ip defined');
-        }
-  
-        if (data.port === undefined) {
-          errors.push('No port defined');
-        }
-  
-        if (data.url === undefined) {
-          errors.push('No url defined');
-        }
-        if (data.messageType === undefined) {
-          errors.push('No message type defined');
-        }
-      } else {
-        errors.push('Problem with request : no body')
+    if (data !== undefined) {
+      if (data.serviceName === undefined) {
+        errors.push('No servicename defined');
       }
 
-      if (errors.length > 0) {
+      if (data.version === undefined) {
+        errors.push('No version defined');
+      }
+
+      if (data.ip === undefined) {
+        errors.push('No ip defined');
+      }
+
+      if (data.port === undefined) {
+        errors.push('No port defined');
+      }
+
+      if (data.url === undefined) {
+        errors.push('No url defined');
+      }
+      if (data.messageType === undefined) {
+        errors.push('No message type defined');
+      }
+    } else {
+      errors.push('Problem with request : no body')
+    }
+
+    if (errors.length > 0) {
+      response.status(422);
+      response.json(errors);
+    } else {
+      data.uuid = uuidv4();
+      const isAdded = Services.add(data);
+      if (isAdded) {
+        response.status(201);
+        response.json({
+          serviceName: global.serviceName,
+          version: global.version,
+          uuid: data.uuid
+        });
+      } else {
+        const uuid = Services.getServiceUUIDByIpAndPort(data.ip, data.port);
+        errors.push('This instance already registered by this UUID : ' + uuid)
         response.status(422);
         response.json(errors);
-      } else {
-        data.uuid = uuidv4();
-        const isAdded = Services.add(data);
-        if (isAdded) {
-          response.status(201);
-          response.json({
-            serviceName: global.serviceName,
-            version: global.version,
-            uuid: data.uuid
-          });
-        } else {
-          const uuid = Services.getServiceUUIDByIpAndPort(data.ip, data.port);
-          errors.push('This instance already registered by this UUID : ' + uuid)
-          response.status(422);
-          response.json(errors);
-        }
       }
+    }
   }
 }

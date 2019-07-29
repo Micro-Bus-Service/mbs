3c2366b9c98918ddc1ced5fe6b3dac8f4d704f9b
Vilsafur <vilsafur@gmail.com>
2019-07-29 17:54:22 +0200

    test: best separation

--- tests/service.test.ts
+++ tests/service.test.ts
@@ -23,47 +23,59 @@ describe("Service store", () => {
   beforeEach(() => {
     sv = new Services();
   });
-  it("should add a service in store", () => {
-    const isAdded = sv.add(data);
 
-    expect(isAdded).to.equal(true);
+  describe("add", () => {
+    it("should add a service in store", () => {
+      const isAdded = sv.add(data);
+
+      expect(isAdded).to.equal(true);
+    });
+
+    it("should got an error if add 2 services with same ip and port", () => {
+      sv.add(data);
+      const isAdded = sv.add(data);
+
+      expect(isAdded).to.equal(false);
+    });
   });
-  it("should got an error if add 2 services with same ip and port", () => {
-    sv.add(data);
-    const isAdded = sv.add(data);
 
-    expect(isAdded).to.equal(false);
+  describe("getByMessageType", () => {
+    it("should return a list of Service for message type given", () => {
+      sv.add(data);
+      const servicesList = sv.getByMessageType("message.test");
+
+      expect(Object.keys(servicesList).length).to.be.equal(1);
+    });
+
+    it("should return an empty list of Service for message type given", () => {
+      sv.add(data);
+      const servicesList = sv.getByMessageType("message.test3");
+
+      expect(Object.keys(servicesList).length).to.be.equal(0);
+    });
   });
-  it("should return a list of Service for message type given", () => {
-    sv.add(data);
-    const servicesList = sv.getByMessageType("message.test");
 
-    expect(Object.keys(servicesList).length).to.be.equal(1);
-  });
-  it("should return an empty list of Service for message type given", () => {
-    sv.add(data);
-    const servicesList = sv.getByMessageType("message.test3");
+  describe("delete", () => {
+    it("should remove a service", () => {
+      sv.add(data);
+      let servicesList = sv.getByMessageType("message.test");
 
-    expect(Object.keys(servicesList).length).to.be.equal(0);
-  });
-  it("should remove a service", () => {
-    sv.add(data);
-    let servicesList = sv.getByMessageType("message.test");
+      expect(Object.keys(servicesList).length).to.be.equal(1);
+      const isDeleted = sv.delete(data.uuid);
+      servicesList = sv.getByMessageType("message.test");
+      expect(isDeleted).to.equal(true);
 
-    expect(Object.keys(servicesList).length).to.be.equal(1);
-    const isDeleted = sv.delete(data.uuid);
-    servicesList = sv.getByMessageType("message.test");
-    expect(isDeleted).to.equal(true);
+      expect(Object.keys(servicesList).length).to.be.equal(0);
+    });
 
-    expect(Object.keys(servicesList).length).to.be.equal(0);
-  });
-  it("should return false if service already remove", () => {
-    sv.add(data);
-    sv.delete(data.uuid);
-    const isDeleted = sv.delete(data.uuid);
-    const servicesList = sv.getByMessageType("message.test");
+    it("should return false if service already remove", () => {
+      sv.add(data);
+      sv.delete(data.uuid);
+      const isDeleted = sv.delete(data.uuid);
+      const servicesList = sv.getByMessageType("message.test");
 
-    expect(Object.keys(servicesList).length).to.be.equal(0);
-    expect(isDeleted).to.equal(false);
+      expect(Object.keys(servicesList).length).to.be.equal(0);
+      expect(isDeleted).to.equal(false);
+    });
   });
 });

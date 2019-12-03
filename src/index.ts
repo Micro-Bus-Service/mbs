2d1f6a2d20a91fb2b8a7f2594db6aff5c35e2565
Cyril POIDEVIN <vilsafur@gmail.com>
2019-12-03 19:51:41 +0100

    refactor: remove unused log

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
 import Server from "@/utils/server";
 
 import pjson from "../package.json";
-import logger from "./utils/logger.js";
 
 const port = 9000;
 
 global.serviceName = "bus";
 global.version = "v" + pjson.version;
-
-logger.info("Start MBS in version " + global.version);
 
 // Instanciate a new Server
 const server = new Server(port);
